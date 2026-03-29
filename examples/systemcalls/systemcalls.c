1369537ffcb8565ad762f947f2fb7a7f791e09f8
Avi Peer <avipeeree@gmail.com>
2026-03-29 13:30:21 +0300

    changed systemcalls.c

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,7 +16,10 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
+    int result = system(cmd);
+    if(result == -1){
+        return false;
+    }
     return true;
 }
 
